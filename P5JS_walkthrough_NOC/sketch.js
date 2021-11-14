8e63a1f0f0ab5d5fc73050461cf3210e876d1ea5
Timi <timi.ogunkeye@gmail.com>
2021-11-14 20:35:00 +0000

    3.6 Simple Harmonic Motion
    Signed-off-by: Timi <timi.ogunkeye@gmail.com>

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -17,11 +17,12 @@ function setup() {
   createCanvas(400, 400);
   // angleMode(RADIANS);
   attractor = new Attractor();
-  background(200,20);
+  // background(200,20);
   
 }
 
 function draw() {
+  background(200,20);
   
   
   if(keyIsDown(LEFT_ARROW)){
@@ -31,19 +32,19 @@ function draw() {
   }
   
   
-   movers.forEach((mover,i)=>{ 
+//    movers.forEach((mover,i)=>{ 
      
-    // gravityBehaviour(mover);
-    // followMouseBehaviour(mover);
-    // attractionBehaviour(attractor, mover);
-    attractor.display();
-    // frictionBehaviour(mover);
+//     gravityBehaviour(mover);
+//     followMouseBehaviour(mover);
+//     attractionBehaviour(attractor, mover);
+//     attractor.display();
+//     frictionBehaviour(mover);
 
      
 //     mover.update();
 //     mover.checkEdges();
 //     mover.display();
-   })
+//    });
   
   //draw baton
 //     stroke(10);
@@ -60,23 +61,36 @@ function draw() {
   // Angular equivalent of location.add(velocity);
 
   
+//   // 3.5 spiral
+//   push();
+//     translate(width/2,height/2);
+//     stroke(255,0,200);
+//     strokeWeight(4);
+//     noFill();
+//     circle(0,0,r*2);
+  
+  
+//     strokeWeight(34);
+//     stroke(255,230,33);
+//     let x = r * cos(angle);
+//     let y = r * sin(angle);
+//     point(x,y);
+//     angle +=0.01;
+//     r -=  0.1;
+//   pop();
+  
+  
+  //// 3.6 Simple Harmonic Motion
+  let period = 120;
+  let amplitude = 100;
+  let x = amplitude * cos(TWO_PI * frameCount / period);
+  
   push();
+    stroke(0);
+    fill(175);
     translate(width/2,height/2);
-    stroke(255,0,200);
-    strokeWeight(4);
-    noFill();
-    circle(0,0,r*2);
-  
-  
-    strokeWeight(34);
-    stroke(255,230,33);
-    let x = r * cos(angle);
-    let y = r * sin(angle);
-    point(x,y);
-    angle +=0.01;
-    r -=  0.1;
-  
+    line(0,0,x,0);
+    ellipse(x,0,20,20);
   pop();
-  
   
 }
