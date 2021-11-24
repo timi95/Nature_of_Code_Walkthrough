bf2e05effee5f7a870c34659f84e883a3638c8e9
Timi <timi.ogunkeye@gmail.com>
2021-11-24 21:24:59 +0000

    Pendulum finally 3.9 working; flip location.y with -1

--- P5JS_walkthrough_NOC/Pendulum.js
+++ P5JS_walkthrough_NOC/Pendulum.js
@@ -2,36 +2,36 @@ class Pendulum {
   
   constructor(origin_, r_){
     this.r=15; //Length of arm
-    this.angle=0; //Pendulum arm angle
+    this.angle=0.00001; //Pendulum arm angle
     this.aVelocity=0; //Angular velocity
-    this.aAcceleration = 0.001; //Angular acceleration
+    this.aAcceleration = 0.0001; //Angular acceleration
+    
+
+    
     this.location = createVector();
-    
     this.damping = 0.995;
-    
-    this.origin = origin_? origin_: createVector(100,10);
+    this.origin = origin_? origin_: createVector(windowWidth/2,150);
     this.r = r_? r_: 125;
   }
   
-  update() {
-    let gravity = 0.4; /*Arbitrary constant
+  update() { 
+    let gravity = 1; /*Arbitrary constant
     Calculate acceleration according to our
     formula.*/
     let force = gravity * sin(this.angle);
-    
-    this.aAcceleration = (-1 * force) /this.r;
+    this.aAcceleration = (force) /this.r;
+ 
     this.aVelocity += this.aAcceleration; //Increment velocity.
     this.angle += this.aVelocity; //Increment angle.
     this.aVelocity *= this.damping;
-    
+
+    this.location.set(
+      this.r*sin(this.angle)+this.origin.x, 
+     -1*this.r*cos(this.angle)+this.origin.y,0);
+
   }
   
   display(){
-    this.location.set(
-      this.r*sin(this.angle)+this.origin.x, 
-      this.r*cos(this.angle)+this.origin.y,0);
-    this.location.add(this.origin);
-    
     stroke(0);
     fill(175);
     line(this.origin.x, this.origin.y, this.location.x, this.location.y);
