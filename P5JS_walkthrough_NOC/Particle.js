18b1a6fea9f1a6621259b3c7d925a179b26771c2
Timi <timi.ogunkeye@gmail.com>
2021-12-06 21:27:57 +0000

    4.1 exercise applyForce added; 4.2 exercise add angular velocity (WIP: rotation not in particles center)

--- P5JS_walkthrough_NOC/Particle.js
+++ P5JS_walkthrough_NOC/Particle.js
@@ -1,13 +1,40 @@
 class Particle {
   
   constructor(_location){
-    this.location = _location? _location: createVector();
+    this.location = _location? 
+      _location: createVector();
     this.velocity = createVector();
     this.acceleration = createVector(0,0.05);
+    
+    this.angle = 0;
+    this.aVelocity = 0;
+    this.aAcceleration = 0.001;
+    
     this.lifespan = 255;
+    this.mass = 1
   }
   
+  
+  applyForce(force){
+    //Making a copy of the PVector before using it!
+     const f = p5.Vector.div(force, this.mass)/* Newton’s second law (with force accumulation and mass) */
+    this.acceleration.add(f);
+  }
+   
+  spin(){
+      rotate(this.angle);
+      this.aVelocity += this.aAcceleration;
+      this.angle += this.aVelocity
+   }
+  
+  
   update(){
+    push();
+      // rectMode(CENTER);
+      // translate(this.location.x, this.location.y);
+      this.spin();
+      this.display();
+    pop();  
     this.velocity.add(this.acceleration);
     this.location.add(this.velocity);
     this.lifespan -= 2.0
@@ -15,7 +42,7 @@ class Particle {
   
    run() {
     this.update();
-    this.display();
+
   }
   
   isDead() {
@@ -32,6 +59,6 @@ class Particle {
     // Since our life ranges from 255 to 0 we can use it for alpha
     stroke(0,this.lifespan);
     fill(175, this.lifespan);
-    ellipse(this.location.x, this.location.y,10,10);
+    rect(this.location.x, this.location.y,70,10);
   }
 }
