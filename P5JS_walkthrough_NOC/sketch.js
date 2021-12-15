a1b948d63945723b63508524e6edeac369648bd1
Timi <timi.ogunkeye@gmail.com>
2021-12-15 20:52:21 +0000

    Append particle in every frame; reverse travel list to spilce dead particles;

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -2,12 +2,7 @@ let particles = [];
 function setup() { 
   createCanvas(windowWidth, 400);
 
-  for(let i =0; i<10; i++) {
-    particles.push (
-      new Particle( createVector(width/2, 50))
-    );
-  }
-  
+
   // ps = new ParticleSystem(createVector(width/2, 50),3);
    
   // ps.setup();
@@ -17,10 +12,20 @@ function draw() {
   background(255);
   createCanvas(windowWidth, 400);
   
+    for(let i =0; i<5; i++) {
+    particles.push (
+      new Particle( createVector(width/2, 50))
+    );
+  }
+  
+  
   // console.log(particles.length);
   particles.forEach(p=>{
-  p.run();
-  if( p.isDead() ) { particles.pop(p); }
+    p.run();
   });
+  
+  for(let i = particles.length-1; i >=0; i--)
+    if( particles[i].isDead() ) 
+    { particles.splice(i,1); }
   // ps.run();
 }
