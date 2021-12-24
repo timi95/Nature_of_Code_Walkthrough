d7e89f4e34ef1391a3ef2ae6c4a025a8a2a90639
Timi <timi.ogunkeye@gmail.com>
2021-12-24 21:54:23 +0000

    rgb coloured particles

--- P5JS_walkthrough_NOC/RectangleP.js
+++ P5JS_walkthrough_NOC/RectangleP.js
@@ -10,13 +10,19 @@ class RectangleP extends Particle {
 
   
   display(){
+    let theta = map(this.location.x,0,width,0,TWO_PI*2);
     angleMode(DEGREES);
     push();
         rectMode(CENTER);
+
         translate(this.location.x, this.location.y);
         rotate(this.angle);
         stroke(0,this.lifespan);
-        fill(175, this.lifespan);
+        fill(
+          this.colour.r,
+          this.colour.g,
+          this.colour.b, 
+             this.lifespan);
         rect(0, 0,this.r*2,this.r);
     pop(); 
 
