80df44eebedd2b1b7773e3115803af995332cc75
Timi <timi.ogunkeye@gmail.com>
2022-01-11 22:13:29 +0000

    just experimenting with random target, too fast for now;

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,29 +1,31 @@
+let angle;
+let xoff;
+let yoff;
 function setup() { 
   createCanvas(windowWidth, 400);
   pursuer = new Vehicle(width/2, height/2);
   target = new Vehicle(random(width), random(height));
+  angle = 0;
+  xoff=0;
+  yoff=1000;
 }
 
 function draw() {
   createCanvas(windowWidth, 400);
-  background(200,20);
+  background(200,200);
   
-   let distance = p5.Vector.dist(pursuer.location, target.location);
-  if (distance < pursuer.r + target.r) {
-    target = new Vehicle(random(width), random(height));
-    pursuer.location.set(width / 2, height / 2);
-  }
-  pursuer.update();
-    pursuer.applyForce(
-    pursuer.seek(
-     target.location, true));  
+//   pursuer.update();
+//     pursuer.applyForce(
+//     pursuer.seek(
+//      target.location, true));  
+  
     // pursuer.applyForce(
     // pursuer.pursue(
     //   target));
   // pursuer.applyForce(
   //   pursuer.seek(
   //     target.location));
-  pursuer.display();
+  // pursuer.display();
 
 //   target.update();
 //   // target.applyForce(
@@ -33,6 +35,27 @@ function draw() {
 //   target.evade(
 //    pursuer));  
 //   target.display();
+  
+  xoff+=random(-1,0.01);
+  // yoff+=0.01;
+  thetaX=noise(xoff);
+  // thetaY=noise(yoff);
+  angle+=noise(xoff);
+  let r = 100;
+  let x = (r/2) * cos(angle);
+  let y = (r/2) * sin(angle);
+  
+  strokeWeight(5);
+  stroke(255);
+  noFill();
+  ellipse(target.location.x, target.location.y, r);
+  strokeWeight(15);
+  stroke(255,5,5);
+  push();
+    translate(target.location.x, target.location.y);
+    point(x,y);
+  pop();
+  
   
   target.update();
   target.applyForce(
