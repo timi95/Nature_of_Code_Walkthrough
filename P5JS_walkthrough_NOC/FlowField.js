761a384612d811eb21d8884626feb1e50429e8fd
Timi <timi.ogunkeye@gmail.com>
2022-01-15 23:21:46 +0000

    6.6 flow field class under construction

---
+++ P5JS_walkthrough_NOC/FlowField.js
@@ -0,0 +1,31 @@
+class FlowField{
+  
+  constructor(r){
+    this.resolution = r;
+    this.cols = width/this.resolution;
+    this.rows = height/this.resolution;
+    this.field = new PVector[this.cols][this.rows];// this is wrong for p5 js
+    this.init();
+  }
+  
+    init() {
+    let xoff = 0;
+    for (let i = 0; i < this.cols; i++) {
+      let yoff = 0;
+      for (let j = 0; j < this.rows; j++) {
+        let theta = map(noise(xoff,yoff),0,1,0,TWO_PI);
+        this.field[i][j] = new PVector(cos(theta),sin(theta));
+        yoff += 0.1;
+      }
+      xoff += 0.1;
+    }
+  }
+  
+    lookup(lookup) {
+ 
+    let column = int(constrain(lookup.x/this.resolution,0,this.cols-1));
+    let row = int(constrain(lookup.y/this.resolution,0,this.rows-1));
+    return this.field[column][row].get();
+  }
+  
+}
