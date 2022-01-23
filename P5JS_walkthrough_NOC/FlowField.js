c5df0980f7aa1ba372d295b5498cab66293b0d83
Timi <timi.ogunkeye@gmail.com>
2022-01-23 20:56:32 +0000

    Bug: vector values reach exponential/infinitesimal size very shortly, and this messes with division operations during applyForce();

--- P5JS_walkthrough_NOC/FlowField.js
+++ P5JS_walkthrough_NOC/FlowField.js
@@ -15,7 +15,7 @@ class FlowField{
       let yoff = 0;
       for (let j = 0; j < this.rows; j++) {
         let theta = map(noise(xoff,yoff),0,1,0,TWO_PI);
-        this.field[i][j] = new createVector(cos(theta),sin(theta));
+        this.field[i][j] = createVector(cos(theta),sin(theta));
         yoff += 0.1;
       }
       xoff += 0.1;
@@ -23,9 +23,29 @@ class FlowField{
   }
   
     lookup(lookup) {
-    let column = int(constrain(lookup.x/this.resolution,0,this.cols-1));
-    let row = int(constrain(lookup.y/this.resolution,0,this.rows-1));
-    return this.field[column][row].get();
+    let column = floor(constrain(this.cols,0,this.cols-1));
+    let row = floor(constrain(this.rows,0,this.rows-1));
+      // console.log('lookup result: ', this.field[column][row] );
+    return  this.field[column][row];
+  }
+  
+  display(){
+    for(let i=0; i<this.field.length;i++){
+      for(let j =0; j< this.field[i].length; j++){
+        
+      }
+    }
+    fill(115);
+    stroke(0);
+    push();
+    translate(this.location.x, this.location.y);
+    rotate(theta);
+    beginShape();
+      vertex(0, -this.r*2);
+      vertex(-this.r, this.r*2);
+      vertex(this.r, this.r*2);
+    endShape(CLOSE);
+    pop();
   }
   
 }
