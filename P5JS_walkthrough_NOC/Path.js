a175a60b0104493a24037ecfda4697ef9bbf05b4
Timi <timi.ogunkeye@gmail.com>
2022-02-06 21:48:05 +0000

    Path class definition

--- P5JS_walkthrough_NOC/Path.js
+++ P5JS_walkthrough_NOC/Path.js
@@ -1,5 +1,16 @@
 class Path{
   constructor(){
-    
+    this.radius = 20;
+    this.start = createVector(0,height/3);
+    this.end = createVector(width,2*height/3);
+  }
+  
+  display() { // Display the path.
+    strokeWeight(this.radius*2);
+    stroke(0,100);
+    line(this.start.x, this.start.y, this.end.x, this.end.y);
+    strokeWeight(1);
+    stroke(0);
+    line(this.start.x, this.start.y, this.end.x, this.end.y);
   }
 }
