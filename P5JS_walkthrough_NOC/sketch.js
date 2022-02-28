a65ca08bddfa9b0fd7eaefc7cf9be085330a0c3e
Timi <timi.ogunkeye@gmail.com>
2022-02-28 22:26:09 +0000

    continuation of chapter 6, 6.10+ complex systems

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,90 +1,9 @@
-let angle;
-let xoff;
-let followers=[]
 
-function mousePressed(){
-  // if(ceil(random(3))==2)
-  path.addPoint(mouseX, mouseY);
-}
 function setup() { 
   createCanvas(windowWidth, 400);
-  // pursuer = new Vehicle(width/2, height/2);
-  // target = new Vehicle(random(width), random(height));
-  path = new Path();
-  angle = 0;
-  xoff=0;
-  createCanvas(windowWidth, 400);
-  background(200,200);
-  
-  for(let i=0; i<3; i++)
-  followers.push(new Vehicle(1, 1));
-    
 }
 
 function draw() {
   createCanvas(windowWidth, 400);
-  flowField = new FlowField(10);
-  background(200,200);
-
-//   pursuer.update();
-//     pursuer.applyForce(
-//     pursuer.seek(
-//      target.location, true));  
   
-    // pursuer.applyForce(
-    // pursuer.pursue(
-    //   target));
-  // pursuer.applyForce(
-  //   pursuer.seek(
-  //     target.location));
-  // pursuer.display();
-
-//   target.update();
-//   // target.applyForce(
-//   // target.flee(
-//   //  pursuer.location));
-//   target.applyForce(
-//   target.evade(
-//    pursuer));  
-//   target.display();
-  
-  
-  // Perlin noise based wonderer
-//   xoff+=0.01;
-//   let r = 100;
-//   thetaX=noise(xoff)*10;
-//   let x = (r/2) * cos(thetaX);
-//   let y = (r/2) * sin(thetaX);
-  
-  //test
-  //illustrative scaffolding
-//   strokeWeight(5);
-//   stroke(255);
-//   noFill();
-//   ellipse(target.location.x, target.location.y, r);
-//   push();
-//   strokeWeight(15);
-//   stroke(255,5,5);
-//     translate(target.location.x, target.location.y);
-//     point(x,y);
-//   pop();
-  
-  
-  // target.update();
-  // target.applyForce(
-  // target.seek(createVector(target.location.x+x, target.location.y+y)));  
-  // target.display();
-  // flowField.display();
-  path.display();
-  followers.forEach(follower=>{
-    follower.update();
-    // follower.applyForce(
-    // follower.seek( createVector(mouseX,mouseY)));
-    follower.followPath(path, true);
-    follower.follow(flowField);
-    follower.display();
-  });
-  // target.update();
-  // target.follow(flowField);  
-  // target.display();
 }
