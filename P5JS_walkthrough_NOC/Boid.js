d83be6eb37cddc789c89a5b6667baf33f56accba
Timi <timi.ogunkeye@gmail.com>
2022-03-14 22:40:08 +0000

    Implemeting Boid class, extends vehicle class

--- P5JS_walkthrough_NOC/Boid.js
+++ P5JS_walkthrough_NOC/Boid.js
@@ -1,3 +1,26 @@
-class Boid {
+class Boid extends Vehicle {
+  constructor(x,y){
+    super(x,y)
+  }
   
+  separate(boids){}
+  
+  align(boids){}
+  
+  cohere(boids){}
+  
+  flock(boids){
+    let sep = separate(boids); //The three flocking rules
+    let ali = align(boids);
+    let coh = cohere(boids);
+    // Arbitrary weights for these forces (Try
+    // different ones!)
+    sep.mult(1.5);
+    ali.mult(1.0);
+    coh.mult(1.0);
+    
+    this.applyForce(sep); //Applying all the forces
+    this.applyForce(ali);
+    this.applyForce(coh);
+  }
 }
