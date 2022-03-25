379745af691da34d9c6a62a43e34f6dea7a235de
Timi <timi.ogunkeye@gmail.com>
2022-03-25 21:15:58 +0000

    reading 7.1, fresh sketch pad

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,26 +1,8 @@
-let vehicles=[];
-let vehicleSystems=[];
-
-let boidFlocks=[]
-
-function mousePressed(){
-  // vehicleSystems.push(new VehicleSystem(mouseX, mouseY));
-  boidFlocks.push(new Flock(mouseX, mouseY));
-}
-
-function setup() { 
+function setup() {
   createCanvas(windowWidth, 400);
 }
 
 function draw() {
   createCanvas(windowWidth, 400);
   background(150);
-  boidFlocks.forEach(flock=>{
-    flock.run();
-  });
-  // vehicleSystems.forEach(vs=>{  
-  //   vs.separate(vs.vehicles);
-  //   vs.update();
-  //   vs.display();
-  // });
 }
