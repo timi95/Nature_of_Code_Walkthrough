6fa07c6f8babd0dcdd5d21543f2e8c8a2ea92d3e
Timi <timi.ogunkeye@gmail.com>
2022-03-28 22:01:39 +0100

    7.3 basic cellular automaton

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,3 +1,4 @@
+const cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
 function setup() {
   createCanvas(windowWidth, 400);
 }
@@ -5,4 +6,11 @@ function setup() {
 function draw() {
   createCanvas(windowWidth, 400);
   background(150);
+  
+  for (let i = 0; i < cells.length; i++) { //Loop through every cell.
+    if (cells[i] == 0) fill(255);
+    else fill(0); //Create a fill based on its state (0 or 1).
+    stroke(0);
+    rect(i*50,0,50,50);
+  }
 }
