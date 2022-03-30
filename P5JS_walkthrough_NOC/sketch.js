abdd9cf22c667e918c2daaf8e5685bbcbd40c819
Timi <timi.ogunkeye@gmail.com>
2022-03-30 22:10:32 +0100

    7.3 implementation of CA, todo: abstract into a class

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,11 +1,30 @@
-const cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
+let cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
+let newcells = new Array(cells.length);
+
+let ruleset = [0,1,1,1,1,0,1,1];
 function setup() {
   createCanvas(windowWidth, 400);
+}
+
+function rules(a, b, c){
+  let s = "" + a + b + c;
+  let index = parseInt(s,2);
+  return ruleset[index];
 }
 
 function draw() {
   createCanvas(windowWidth, 400);
   background(150);
+  
+  
+  for (let i = 1; i < cells.length-1; i++) { //Loop through every cell.
+    let left = cells[i-1]; //Look at the states from the current array.
+    let middle = cells[i];
+    let right = cells[i+1];
+    let newstate = rules(left,middle,right);
+    newcells[i] = newstate; 
+  }
+    cells = newcells;
   
   for (let i = 0; i < cells.length; i++) { //Loop through every cell.
     if (cells[i] == 0) fill(255);
