77d6432851a078b998838e38442b3491a9adb060
Timi <timi.ogunkeye@gmail.com>
2022-04-04 19:39:13 +0100

    initialise CA object in setup, and display in draw method.

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,9 +1,12 @@
-let cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
-let newcells = new Array(cells.length);
+// let cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
+// let newcells = new Array(cells.length);
 
-let ruleset = [0,1,1,1,1,0,1,1];
+// let ruleset = [0,1,1,1,1,0,1,1];
+let cA;
 function setup() {
   createCanvas(windowWidth, 400);
+  
+  cA = new CA();
 }
 
 function rules(a, b, c){
@@ -16,20 +19,21 @@ function draw() {
   createCanvas(windowWidth, 400);
   background(150);
   
+  cA.generate();
+  cA.display();
+//   for (let i = 1; i < cells.length-1; i++) { //Loop through every cell.
+//     let left = cells[i-1]; //Look at the states from the current array.
+//     let middle = cells[i];
+//     let right = cells[i+1];
+//     let newstate = rules(left,middle,right);
+//     newcells[i] = newstate; 
+//   }
+//     cells = newcells;
   
-  for (let i = 1; i < cells.length-1; i++) { //Loop through every cell.
-    let left = cells[i-1]; //Look at the states from the current array.
-    let middle = cells[i];
-    let right = cells[i+1];
-    let newstate = rules(left,middle,right);
-    newcells[i] = newstate; 
-  }
-    cells = newcells;
-  
-  for (let i = 0; i < cells.length; i++) { //Loop through every cell.
-    if (cells[i] == 0) fill(255);
-    else fill(0); //Create a fill based on its state (0 or 1).
-    stroke(0);
-    rect(i*50,0,50,50);
-  }
+//   for (let i = 0; i < cells.length; i++) { //Loop through every cell.
+//     if (cells[i] == 0) fill(255);
+//     else fill(0); //Create a fill based on its state (0 or 1).
+//     stroke(0);
+//     rect(i*50,0,50,50);
+//   }
 }
