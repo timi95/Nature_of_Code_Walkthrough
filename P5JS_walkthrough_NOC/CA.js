28f45d29bb9df532b3399f50831d3016c2438338
Timi <timi.ogunkeye@gmail.com>
2022-05-25 19:21:45 +0100

    Bug fix: initialise Javascript array with .fill(0)

--- P5JS_walkthrough_NOC/CA.js
+++ P5JS_walkthrough_NOC/CA.js
@@ -1,11 +1,10 @@
 class CA {
-  
-  constructor(){
+  constructor(ca_width){
     this.generation=0;
     this.w=5;
-    this.cells = new Array(2000/this.w);
+    this.cells = new Array(1050/this.w).fill(0);
     this.newcells = new Array(this.cells.length);
-    this.ruleset = [0,1,0,1,1,0,1,0];
+    this.ruleset = [0,1,0,1,1,0,1,0];// [1,1,0,1,1,1,1,0]//
     
     for (let i = 0; i < this.cells.length; i++) {
       this.cells[i] = 0;
@@ -16,7 +15,7 @@ class CA {
   
   
   generate() {
-    let nextgen = new Array(this.cells.length);
+    let nextgen = new Array(this.cells.length).fill(0);
     for (let i = 1; i < this.cells.length-1; i++) {
       let left = this.cells[i-1];
       let me = this.cells[i];
@@ -35,7 +34,8 @@ class CA {
   
   display(){
     for (let i = 0; i < this.cells.length; i++) { 
-      if (this.cells[i] == 0) fill(255);
+      if (this.cells[i] == 0) 
+        fill(255);
       else fill(0); 
       stroke(0);
       rect(i*this.w, this.generation*this.w, 
--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -6,36 +6,16 @@ let cA;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  cA = new CA();
+  cA = new CA(1000);
 }
 
-function rules(a, b, c){
-  let s = "" + a + b + c;
-  let index = parseInt(s,2);
-  return ruleset[index];
-}
 
 function draw() {
   // createCanvas(windowWidth, 400);
   // background(150);
   
-  cA.generate();
   cA.display();
+  cA.generate();
 
   
-//   for (let i = 1; i < cells.length-1; i++) { //Loop through every cell.
-//     let left = cells[i-1]; //Look at the states from the current array.
-//     let middle = cells[i];
-//     let right = cells[i+1];
-//     let newstate = rules(left,middle,right);
-//     newcells[i] = newstate; 
-//   }
-//     cells = newcells;
-  
-//   for (let i = 0; i < cells.length; i++) { //Loop through every cell.
-//     if (cells[i] == 0) fill(255);
-//     else fill(0); //Create a fill based on its state (0 or 1).
-//     stroke(0);
-//     rect(i*50,0,50,50);
-//   }
 }
