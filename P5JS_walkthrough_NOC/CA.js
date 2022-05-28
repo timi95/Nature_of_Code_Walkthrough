e5aef207e0eb713b6f321d73c28cfb4157a54cc7
Timi <timi.ogunkeye@gmail.com>
2022-05-28 22:04:48 +0100

    randomly generate new rules and display over time

--- P5JS_walkthrough_NOC/CA.js
+++ P5JS_walkthrough_NOC/CA.js
@@ -5,6 +5,16 @@ class CA {
     this.cells = new Array(1050/this.w).fill(0);
     this.newcells = new Array(this.cells.length);
     this.ruleset = [0,1,0,1,1,0,1,0];// [1,1,0,1,1,1,1,0]//
+    this.randRuleSet = [
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+      random([0,1]),
+    ]
     
     for (let i = 0; i < this.cells.length; i++) {
       this.cells[i] = 0;
@@ -20,7 +30,7 @@ class CA {
       let left = this.cells[i-1];
       let me = this.cells[i];
       let right = this.cells[i+1];
-      nextgen[i] = this.rules(left, me, right);
+      nextgen[i] = this.randomRules(left, me, right);
     }
     this.cells = nextgen;
     this.generation++;
@@ -31,6 +41,12 @@ class CA {
     let index = parseInt(s,2);
     return this.ruleset[index];
   }
+   
+  randomRules(a, b, c){
+    let s = "" + a + b + c;
+    let index = parseInt(s,2);
+    return this.randRuleSet[index];
+  }
   
   display(){
     for (let i = 0; i < this.cells.length; i++) { 
@@ -40,6 +56,21 @@ class CA {
       stroke(0);
       rect(i*this.w, this.generation*this.w, 
            this.w,this.w);
+    }
+    if(this.generation>(105)){
+      this.generation = 0;
+      this.randRuleSet = [
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+        random([0,1]),
+      ]
+
+      
     }
   }
 }
