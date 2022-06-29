938f77bfd9234022f4ef7157fa76167a37c1dcc0
Timi <timi.ogunkeye@gmail.com>
2022-06-29 21:35:14 +0100

    ca2 refactor variable on sketch

--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,8 +1,5 @@
-// let cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
-// let newcells = new Array(cells.length);
-
-// let ruleset = [0,1,1,1,1,0,1,1];
 let cA;
+let ca2;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
