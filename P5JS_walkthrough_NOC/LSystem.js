369da9156776e18659b9d26d3de93aa97f88a49b
Timi <timi.ogunkeye@gmail.com>
2022-10-06 20:20:33 +0100

    Lsystem drawing to screen now

--- P5JS_walkthrough_NOC/LSystem.js
+++ P5JS_walkthrough_NOC/LSystem.js
@@ -1,8 +1,27 @@
 class LSystem {
-  constructor(){
-    this.current = "A";
+  constructor(axiom, ruleset){
+    this.current = axiom?axiom:"A";
     this.next = "";
     this.count=0;
+    this.len = windowWidth;
+    this.theta = radians(25);
+    this.treeAngle = -90
+  }
+  
+  generateFromAxiom(){
+    let i = 0;
+    if (i < this.current.length) {
+      let c = this.current.charAt(i);
+      if(c === 'F'){
+        this.next += "FG+[+F-F-F]-[-F+F+F]";
+      }
+      this.current = this.next;
+    }
+    i+=1;
+  }
+  
+  getSentence(){
+    return this.current;
   }
   
   generate(){
@@ -22,8 +41,33 @@ class LSystem {
         "Generation #" + this.count + ", "+
         // "next: [" + this.next + "], "+
         "current: " + this.current);
-
-    
+  }
+  
+  turtleDraw(){
+    // push();
+    rotate(radians(this.treeAngle))
+      for (let i = 0; i < this.current.length; i++) {
+        let c = this.current.charAt(i);
+        if (c == 'F') {
+          strokeWeight(4);
+          stroke(0);
+          line(0,0,this.len,0);
+          translate(this.len,0);
+        } else if (c == 'G') {
+          translate(this.len,0);
+        } else if (c == '+') {
+          rotate(this.theta);
+        } else if (c == '-') {
+          rotate(-this.theta);
+        } else if (c == '[') {
+          push();
+        } else if (c == ']') {
+          pop();
+        }
+        // console.log(`Length: [${this.len}]\n`);
+      }
+      this.len *= 0.5;
+    // i+=1;
   }
   update(){}
   draw(){
--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,8 +1,11 @@
 let lSys;
+let turtle;
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(150);
 
-  lSys = new LSystem();
+  lSys = new LSystem("F", "FF+[+F-F-F]-[-F+F+F]");
+  // turtle = new Turtle(lsys.getSentence(),width/4,radians(25));
 }
 
 function drawCircle( x,  y,  radius) {
@@ -33,13 +36,19 @@ function drawCircle2( x,  y,  radius) {
 }
 
 function mousePressed(){
-  lSys.generate();
+  // lSys.generate();
+
+  lSys.generateFromAxiom();
+  lSys.turtleDraw();
+  // turtle.setToDo(lsys.getSentence());
+  // turtle.changeLen(0.5); 
 }
 
 function draw() {  
-  createCanvas(windowWidth, windowHeight);
-  background(150);
+  // createCanvas(windowWidth, windowHeight);
   // drawCircle2(width/2,height/4,200);
   // drawCircle(width/2,height-100,200);
+  translate(windowWidth/2, windowHeight+100); //Start at the bottom of the window and draw.
+  // turtle.render();
   
 }
