41363a0cb127b6418f56e90901d70164bf5c4fd2
Timi <timi.ogunkeye@gmail.com>
2022-12-16 21:44:51 +0000

    mating pool

--- P5JS_walkthrough_NOC/DNA.js
+++ P5JS_walkthrough_NOC/DNA.js
@@ -1,12 +1,23 @@
 class DNA {
   constructor(){
+    this.target="to be or not to be";
+    this.fitness=0;
     this.population = new Array(100);
     this.genes = new Array(18);//an array of chars
     // generate
-    for (let i = 0; i < genes.length; i++) {
+    for (let i = 0; i < this.genes.length; i++) {
       this.genes[i] = String.
       fromCharCode(random(32,128));
     }
   }
   
+  funFitness() { //Function to score fitness
+    let score = 0;
+    for (let i = 0; i < this.genes.length; i++) {
+      if (this.genes[i] == this.target.charAt(i)) { 
+        score++;
+      }
+    }
+    this.fitness = (score)/this.target.length;
+  }
 }
--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,18 +1,30 @@
-let population;
+let population=new Array(100);
+let matingPool=[]
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
   
-function setup() {
     for (let i = 0; i < population.length; i++) {
-    population[i] = new DNA(); 
+      population[i] = new DNA(); 
       //Initializing each member of the population
     }
-  }
+
+  console.log(population)
 }
 
 
 function draw() {  
-  createCanvas(windowWidth, windowHeight);
   background(150);
+  
+  for (let i = 0; i < population.length; i++) {
+    population[i].funFitness();
+    // console.log(population[i].fitness)
+  }
+  
+  for (let i = 0; i < population.length; i++) {
+    let n = int(population[i].fitness * 100);
+    for (let j = 0; j < n; j++) {
+      matingPool.push(population[i]);
+    }
+  }
 }
