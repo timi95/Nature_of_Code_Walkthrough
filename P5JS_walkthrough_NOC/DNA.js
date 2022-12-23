1533314ff31062d65578a8b8ae10b778d6c0aa48
Timi <timi.ogunkeye@gmail.com>
2022-12-23 22:47:41 +0000

    Buggy GA

--- P5JS_walkthrough_NOC/DNA.js
+++ P5JS_walkthrough_NOC/DNA.js
@@ -4,6 +4,7 @@ class DNA {
     this.fitness=0;
     this.population = new Array(100);
     this.genes = new Array(18);//an array of chars
+    this.mutationRate = 0.01;
     // generate
     for (let i = 0; i < this.genes.length; i++) {
       this.genes[i] = String.
@@ -19,5 +20,39 @@ class DNA {
       }
     }
     this.fitness = (score)/this.target.length;
+  }
+  
+  crossover(partner) {
+    // The child is a new instance of DNA. Note
+    // that the DNA is generated randomly in the
+    // constructor, but we will overwrite it below
+    // with DNA from parents.
+    let child = new DNA();
+    // Picking a random “midpoint” in the genes
+    // array
+    let midpoint = random(this.genes.length);
+    for (let i = 0; i < this.genes.length; i++) {
+      // Before midpoint copy genes from one
+      // parent, after midpoint copy genes from the
+      // other parent
+      if (i > midpoint) child.genes[i] = this.genes[i];
+      else child.genes[i] = partner.genes[i];
+    }
+    return child;//Return the new child DNA
+  }
+  
+  mutate() {
+      for (let i = 0; i < this.genes.length; i++) { 
+        // Looking at each gene in the array
+      if (random(1) < this.mutationRate) {
+        this.genes[i] = String.
+        fromCharCode(random(32,128))
+        // Mutation, a new random character
+      }
+    }
+  }
+  
+  getPhrase(){
+    return this.genes.join("");
   }
 }
--- P5JS_walkthrough_NOC/sketch.js
+++ P5JS_walkthrough_NOC/sketch.js
@@ -1,5 +1,5 @@
 let population=new Array(100);
-let matingPool=[]
+let matingPool=[];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
@@ -9,22 +9,40 @@ function setup() {
       //Initializing each member of the population
     }
 
-  console.log(population)
 }
 
 
 function draw() {  
   background(150);
-  
   for (let i = 0; i < population.length; i++) {
     population[i].funFitness();
     // console.log(population[i].fitness)
   }
   
+  matingPool=[];
   for (let i = 0; i < population.length; i++) {
     let n = int(population[i].fitness * 100);
     for (let j = 0; j < n; j++) {
       matingPool.push(population[i]);
     }
+  }
+
+
+  for (let i = 0; i < population.length; i++) {
+    let a = int(random(matingPool.length));
+    let b = int(random(matingPool.length));
+
+    let parentA = matingPool[a];
+    let parentB = matingPool[b];
+
+    let child = parentA.crossover(parentB);
+    child.mutate();
+    population[i] = child;
+  }
+  
+  for (let i = 0; i < population.length; i++) {
+    
+  console.log('gene phrase: '
+              +population[i].getPhrase()+'\n');
   }
 }
